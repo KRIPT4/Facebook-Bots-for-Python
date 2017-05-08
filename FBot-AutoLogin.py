87f4351ae15e158b6d8428019bfe128d479c8593
KRIPT4 <danielcsich@gmail.com>
2017-05-08 18:47:49 -0300

    v1.0
    v1.0

---
+++ FBot-AutoLogin.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+FBot-AutoLogin.py - Facebook Bots for Python
+Copyright 2017, KRIPT4
+
+More info:
+ * KRIPT4: https://github.com/KRIPT4/Facebook-Bots-for-Python
+"""
+
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+#https://github.com/SeleniumHQ/selenium/blob/master/py/selenium/webdriver/chrome/options.py
+
+chrome_options = Options()
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--allow-running-insecure-content')
+chrome_options.add_argument('--disable-web-security')
+chrome_options.add_argument('--no-referrers')
+chrome_options.add_argument('--window-size=800,800')
+chrome_options.add_experimental_option('prefs', {
+    'credentials_enable_service': False,
+    'profile': {
+        'password_manager_enabled': False
+    }
+})
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get('https://m.facebook.com/home.php')
+
+time.sleep(5)
+
+varUSER = '==USERNAME=='
+varPASS = '==PASSWORD=='
+
+## LOGIN
+driver.find_element_by_xpath('//*[@id="u_0_1"]/div[1]/div/input').send_keys(varUSER)
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="u_0_2"]').send_keys(varPASS)
+time.sleep(3)
+driver.find_element_by_xpath('//*[@id="u_0_6"]').click()
+## END LOGIN
---
+++ FBot-PageFanInvite.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""
+FBot-PageFanInvite.py - Facebook Bots for Python
+Copyright 2017, KRIPT4
+
+More info:
+ * KRIPT4: https://github.com/KRIPT4/Facebook-Bots-for-Python
+"""
+
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+#https://github.com/SeleniumHQ/selenium/blob/master/py/selenium/webdriver/chrome/options.py
+
+chrome_options = Options()
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--allow-running-insecure-content')
+chrome_options.add_argument('--disable-web-security')
+chrome_options.add_argument('--no-referrers')
+chrome_options.add_argument('--window-size=800,800')
+chrome_options.add_experimental_option('prefs', {
+    'credentials_enable_service': False,
+    'profile': {
+        'password_manager_enabled': False
+    }
+})
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get('https://m.facebook.com/home.php')
+
+time.sleep(5)
+
+varUSER = '==USERNAME=='
+varPASS = '==PASSWORD=='
+i = 1 #START
+c = 5 #END
+
+## LOGIN
+driver.find_element_by_xpath('//*[@id="u_0_1"]/div[1]/div/input').send_keys(varUSER)
+time.sleep(2)
+driver.find_element_by_xpath('//*[@id="u_0_2"]').send_keys(varPASS)
+time.sleep(3)
+driver.find_element_by_xpath('//*[@id="u_0_6"]').click()
+## END LOGIN
+
+time.sleep(2)
+
+## SEND FAN PAGE INVITE
+driver.get('https://m.facebook.com/send_page_invite/?pageid=262588213843476')
+time.sleep(7)
+
+while (i < c):
+	valuef = '//*[@id="m_invitee_list"]/div[' + str(i) + ']/div[3]/div/div/button'
+	driver.find_element_by_xpath(valuef).click()
+	time.sleep(2) #OPTIONAL
+	i += 1
+print("END SEND LIKE")
+## END SEN FAN PAGE INVITE
